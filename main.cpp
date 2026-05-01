2ea28e095d2e832ccadabe68ab51f5f849a92a17
Iyana0626 <l250626@lhr.nu.edu.pk>
2026-05-01 20:11:14 +0500

    added reviews & ratings feature

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include<iostream>
 #include"features/bookmanagement.cpp"
+#include "features/reviews&ratings.cpp"
 
 int main()
 {
+	//bookmanager 
 	bookmanager obj;
 	obj.menu(true);
+
+	//reviews & ratings
+	review reviews[MAX_REVIEWS];
+	int count = 0;
+
+	loadreviews(reviews, count);
+
+	string role;
+	cout << "Login as (admin/member): ";
+	cin >> role;
+
+	while (role != "Admin" && role != "admin" && role != "member" && role != "Member")
+	{
+		cout << "Invalid entry. Try again: ";
+		cin >> role;
+	}
+	bool isAdmin;
+	if (role == "Admin" || role == "admin")
+	{
+		isAdmin = true;
+	}
+
+	reviewmenu(reviews, count, isAdmin);
 	return 0;
 }

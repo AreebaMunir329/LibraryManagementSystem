a427896789d1b9c96c314a09b5309b3107553219
Iyana0626 <l250626@lhr.nu.edu.pk>
2026-05-02 20:26:04 +0500

    removed VS project from repo

--- features/reviews&ratings.cpp
+++ features/reviews&ratings.cpp
@@ -368,32 +368,4 @@ void reviewmenu(review revs[], int& count, bool isAdmin)
 	cout << "---------------------------------------" << endl;
 }
 
-int main()
-{
-	review reviews[MAX_REVIEWS];
-		int count = 0;
-	
-		loadreviews(reviews, count);
-	
-		string role;
-		cout << "Login as (admin/member): ";
-		cin >> role;
-	
-		while (role != "Admin" && role != "admin" && role != "member" && role != "Member")
-		{
-			cout << "Invalid entry. Try again: ";
-			cin >> role;
-		}
-		bool isAdmin;
-		if (role == "Admin" || role == "admin")
-		{
-			isAdmin = true;
-		}
-		else
-		{
-			isAdmin = false;
-		}
-	
-		reviewmenu(reviews, count, isAdmin);
-		return 0;
-}
+

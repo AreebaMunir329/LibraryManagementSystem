c01835e2dfe92bacf207fc84bab7b76517eae209
Iyana0626 <l250626@lhr.nu.edu.pk>
2026-05-10 19:23:16 +0500

    update bookmanagement

--- features/bookmanagement.cpp
+++ features/bookmanagement.cpp
@@ -1,38 +1,107 @@
-#ifndef SETTINGS_AND_LEADERBOARD_CPP
-#define SETTINGS_AND_LEADERBOARD_CPP
+#ifndef BOOK_MANAGEMENT_CPP
+#define BOOK_MANAGEMENT_CPP
 
 #include <fstream>
+#include <string>
 using namespace std;
 
-class Settings
+class Book
 {
+    int id;
+    string title;
+    string author;
+    string isbn;
+    string category;
+    int totalCopies;
+    int availableCopies;
+    int timesIssued;
+
 public:
-    double fineRate;
-    int maxBooks;
-    int reservationDays;
-
-    Settings()  //default settings
+    Book()
     {
-        fineRate = 10;
-        maxBooks = 3;
-        reservationDays = 3;
+        id = 0;
+        title = "";
+        author = "";
+        isbn = "";
+        category = "";
+        totalCopies = 0;
+        availableCopies = 0;
+        timesIssued = 0;
     }
 
-    void load()
+    void setData(int i, string t, string a, string is, string c, int total, int avail, int issued)
     {
-        ifstream in("settings.txt");
-        if (!in)
-            return;
-
-        in >> fineRate >> maxBooks >> reservationDays;
-        in.close();
+        id = i;
+        title = t;
+        author = a;
+        isbn = is;
+        category = c;
+        totalCopies = total;
+        availableCopies = avail;
+        timesIssued = issued;
     }
 
-    void save()
+    int getId()
     {
-        ofstream out("settings.txt");
-        out << fineRate << " " << maxBooks << " " << reservationDays << endl;
-        out.close();
+        return id;
+    }
+
+    string getTitle()
+    {
+        return title;
+    }
+
+    string getAuthor()
+    {
+        return author;
+    }
+
+    string getIsbn()
+    {
+        return isbn;
+    }
+
+    string getCategory()
+    {
+        return category;
+    }
+
+    int getAvailable()
+    {
+        return availableCopies;
+    }
+
+    int getTotal()
+    {
+        return totalCopies;
+    }
+
+    int getTimesIssued()
+    {
+        return timesIssued;
+    }
+
+    bool issue()
+    {
+        if (availableCopies <= 0)
+            return false;
+
+        availableCopies--;   //reduces available copies by 1
+        timesIssued++;   //increments total issues for the person
+        return true;
+    }
+
+    void returnCopy()
+    {
+        if (availableCopies < totalCopies)
+            availableCopies++;
+    }
+
+    void save(ofstream& out)
+    {
+        out << id << "|" << title << "|" << author << "|" << isbn << "|"
+            << category << "|" << totalCopies << "|" << availableCopies << "|"
+            << timesIssued << endl;
     }
 };
 
